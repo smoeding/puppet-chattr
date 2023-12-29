3d75cd616b6c1052c2d97781f80a790b21320ed7
Stefan Möding <stm@kill-9.net>
2023-12-29 16:36:32 +0100

    Add unit tests for type

---
+++ spec/types/chattr_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'chattr' do
+  context 'with absolute path' do
+    let(:title) { '/foo' }
+
+    it {
+      is_expected.to be_valid_type.with_provider(:linux)
+
+      is_expected.to be_valid_type.with_properties('immutable')
+      is_expected.to be_valid_type.with_properties('append_only')
+    }
+  end
+end
